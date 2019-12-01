29be26e22867b55a0bfa89718317182e8b6ce76d
Jörg Eisfeld <github@jeisfeld.de>
2019-12-01 23:29:43 +0100

    Added button listeners to TadelTest

--- LuT/src/de/jeisfeld/pi/lut/TadelTest.java
+++ LuT/src/de/jeisfeld/pi/lut/TadelTest.java
@@ -3,6 +3,7 @@ package de.jeisfeld.pi.lut;
 import java.io.IOException;
 
 import de.jeisfeld.pi.lut.core.ButtonStatus;
+import de.jeisfeld.pi.lut.core.ButtonStatus.ButtonListener;
 import de.jeisfeld.pi.lut.core.ChannelSender;
 import de.jeisfeld.pi.lut.core.Sender;
 
@@ -10,6 +11,11 @@ import de.jeisfeld.pi.lut.core.Sender;
  * Test class for LuT framework.
  */
 public class TadelTest { // SUPPRESS_CHECKSTYLE
+	/**
+	 * Flag indicating if the Tadel is active.
+	 */
+	private static boolean mIsActive = true;
+
 	/**
 	 * Main method.
 	 *
@@ -32,14 +38,48 @@ public class TadelTest { // SUPPRESS_CHECKSTYLE
 		Sender sender = Sender.getInstance();
 		ChannelSender channelSender = sender.getChannelSender(1);
 
+		sender.setButton1Listener(new ButtonListener() {
+			@Override
+			public void handleButtonUp() {
+				// do nothing
+			}
+
+			@Override
+			public void handleButtonDown() {
+				TadelTest.mIsActive = !TadelTest.mIsActive;
+			}
+		});
+
+		sender.setButton2Listener(new ButtonListener() {
+			@Override
+			public void handleButtonUp() {
+				// do nothing
+			}
+
+			@Override
+			public void handleButtonDown() {
+				try {
+					Runtime.getRuntime().exec("sudo shutdown -h now");
+				}
+				catch (IOException e) {
+					// ignore
+				}
+			}
+		});
+
 		while (true) {
 			ButtonStatus status = sender.getButtonStatus();
 			int power = status.getControl1Value();
-			int frequency = status.getControl2Value();
-			int wave = status.getControl3Value();
+			int frequency = status.getControl2Value() / 2;
+			int wave = status.getControl3Value() / 10; // MAGIC_NUMBER
 
 			System.out.println(power + " - " + frequency + " - " + wave);
-			channelSender.tadel(power, frequency, wave, Sender.SEND_DURATION);
+			if (TadelTest.mIsActive) {
+				channelSender.tadel(power, frequency, wave, Sender.SEND_DURATION);
+			}
+			else {
+				channelSender.tadel(0, 0, 0, Sender.SEND_DURATION);
+			}
 		}
 	}
 
